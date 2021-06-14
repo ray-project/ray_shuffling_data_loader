724e33b948a70eab4487c201079f16e1d658876c
Clark Zinzow <clarkzinzow@gmail.com>
2021-06-14 19:28:44 +0000

    Revert "Add debug logging to batch queue and shuffler."
    This reverts commit 7cd5ad98f8eb971411aa7ab0af54069c7a1cffbe.

--- ray_shuffling_data_loader/batch_queue.py
+++ ray_shuffling_data_loader/batch_queue.py
@@ -391,7 +391,6 @@ class _QueueActor:
     async def producer_done(self, rank: int, epoch: int):
         await self.queues[epoch][rank].put(None)
         self.queue_producer_done[epoch][rank].set()
-        print(f"Producer for trainer {rank} and epoch {epoch} is done.")
 
     async def wait_until_all_epochs_done(self):
         await asyncio.wait([
